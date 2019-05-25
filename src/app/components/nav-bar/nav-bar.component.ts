e7656b4e46dd333c89865a648d86bc0c49bd2a45
Austin Roa <ausroa@gmail.com>
2019-05-24 22:14:31 -0400

    navbar update

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,12 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { useAnimation } from '@angular/animations';
+import { slideInRight } from 'ng-animate';
 
 @Component({
   selector: 'nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
+  animations: [
+    trigger('openClose', [
+      // ...
+      state('open', style({
+        opacity: 1
+      })),
+      state('closed', style({
+        width: 0
+      })),
+      transition('open => closed', [
+        useAnimation(slideInRight)
+      ]),
+      transition('closed => open', [
+        useAnimation(slideInRight)
+      ]),
+    ]),
+  ],
 })
 export class NavBarComponent implements OnInit {
   public links: any[];
+  public isOpen: boolean;
 
   constructor() { }
 
@@ -19,6 +40,11 @@ export class NavBarComponent implements OnInit {
       {linkName: 'Contact', url: ''},
       {linkName: 'Social', url: ''}
     ];
+
+    this.isOpen = false;
   }
 
+  toggleSearch() {
+    this.isOpen = !this.isOpen;
+  }
 }
