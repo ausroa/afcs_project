a574c78115ee61b45678e9d7ca5f6d8f3d3432b8
Austin Roa <ausroa@gmail.com>
2019-05-29 01:59:01 -0400

    swiper layout fixed

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -10,12 +10,13 @@ export class SliderComponent implements OnInit {
 
   config: SwiperOptions = {
     autoplay: 3000, // Autoplay option having value in milliseconds
-    initialSlide: 0, // Slide Index Starting from 0
+    initialSlide: 2, // Slide Index Starting from 0
     slidesPerView: 3, // Slides Visible in Single View Default is 1
     pagination: '.swiper-pagination', // Pagination Class defined
     paginationClickable: true, // Making pagination dots clicable
     nextButton: '.swiper-button-next', // Class for next button
     prevButton: '.swiper-button-prev', // Class for prev button
+    centeredSlides: true
   };
 
   constructor() { }
